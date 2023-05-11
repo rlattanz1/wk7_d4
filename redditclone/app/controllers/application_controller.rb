4db8eb2669225744b93fb925bd5a6534007353a0
ertan3 <ernesttan96@gmail.com>
2023-05-11 10:38:19 -0700

    completed application controller

--- redditclone/app/controllers/application_controller.rb
+++ redditclone/app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+    #HCRLLL
+
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        return nil if session[:session_token].nil?
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def required_logged_in
+        redirect_to new_session_url unless current_user
+    end
+
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+        @current_user = user
+    end
+
+    def logout!
+        current_user.reset_session_token!
+        @current_user = nil
+        session[:session_token] = nil
+    end
+
+    def logged_in?
+        !!current_user
+    end
 end
--- redditclone/app/controllers/users_controller.rb
+++ redditclone/app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
-    before_action :required_logged_in, only: [:update, :destroy]
+    before_action :required_logged_in, only: [:show, :index, :update, :destroy]
 
+    def index
+        @users = User.all
+        render :index
+    end
+
+    def show
+        @user = User.find_by(:id params[:id])
+        render :show
+    end
+    
     def edit
         @user = User.find_by(id: params[:id])
         render :edit
@@ -24,6 +34,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            login!(@user)
             redirect_to users_url
         else
             flash.now[:errors] = @user.error.full_messages
@@ -33,7 +44,8 @@ class UsersController < ApplicationController
 
     def destroy
         @user = User.find_by(id: params[:id])
-        
+        @user.destroy
+        redirect_to new_session_url
     end
 
     private
