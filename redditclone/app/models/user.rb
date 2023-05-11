453ca32d3b9d38945e0c41b94db3c4983212a3f2
ertan3 <ernesttan96@gmail.com>
2023-05-11 09:43:39 -0700

    completed User model

---
+++ redditclone/app/models/user.rb
@@ -0,0 +1,52 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  session_token   :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+    validates :username, :session_token, :password_digest, presence: true
+    validates :username, :session_token, uniqueness: true
+    validates :password, length: {minimum: 4}, allow_nil: true
+
+    before_validation :ensure_session_token
+
+    attr_reader :password
+
+    #SPIRE
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_valid_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_valid_password?(password)
+        password_obj = BCrypt::Password.new(self.password_digest)
+        password_obj.is_valid_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
+
+end
